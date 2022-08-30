f9e195cae4b01040e31813c2a155870b2aa81334
Eli <+>
2022-08-30 13:41:20 -0400

    commit tree bd1c4db102959d931733dd06347b277854d239a3

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :genre, :name, :artist
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    @@genre_count = {}
+    @@artist_count = {}
+
+    def initialize(name, artist, genre)
+        @genre = genre
+        @artist = artist
+        @name = name
+        @@count += 1
+        @@genres << self.genre
+        @@artists << self.artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+
+end
+
